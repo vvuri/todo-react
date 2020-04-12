abae8f5e8a17a3482301d0325907abf3aff85f69
Yuri Vinogradov <vvuri1978@gmail.com>
2020-04-12 13:26:06 +0300

    Add Items text

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -2,16 +2,41 @@ import React, { Component } from 'react';
 import './AddItem.css';
 
 export default class AddItem extends Component {
+    state = {
+        label: ''
+    }
+    
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    }
+
+    onSubmit = (e) => {
+        /* отменить стандартно поведение формы - перезагрузка */
+        e.preventDefault(); 
+        if ( this.state.label !== '' ) 
+            this.props.onAdd(this.state.label);
+        this.setState({
+            label: ''
+        });
+    }
 
     render () {    
-        const { onAdd } = this.props;
-
         return (
-            <button type="button" 
-                className="btn btn-info additem-button"
-                onClick = { () => onAdd('Drink Coffee') }>
-                Add Item
-            </button>
+            <form className="add-item d-flex"
+                onSubmit = { this.onSubmit }>
+                <input type="text" 
+                    className="form-control" 
+                    onChange={ this.onLabelChange } 
+                    placeholder = "Whats you do"
+                    value = { this.state.label } />
+                <button type="button" 
+                    className="btn btn-info additem-button"
+                    onClick = { this.onSubmit }>
+                    AddItem
+                </button>
+            </form>
         );
     }
 }
